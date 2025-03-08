1743379a2339323ae66eab7c74b1bc1439936ec6
Nithin S J <nithinjayanjasla@gmail.com>
2025-03-08 20:17:39 +0530

    server ai cors api update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://jopapp-0gt0.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
