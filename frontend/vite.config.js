294c848fd333f0669baf4fdbc5073cc510e1555b
Nithin S J <nithinjayanjasla@gmail.com>
2025-03-08 20:22:42 +0530

    fornent  api update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000, // Ensures the dev server runs on port 3000
+    port: 8000, // Ensures the dev server runs on port 3000
     hmr: {
       host: "localhost", // Ensures Hot Module Replacement works correctly
     },
